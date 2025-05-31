fbdf623490ec5357aeea1fe8f0769045c9b24010
Pranabudhathoki <pranabudhathoki@gmail.com>
2025-05-31 08:36:33 +0545

    Add Circle.java

---
+++ Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+    private double radius;
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5.0);
+        
+        System.out.println("Circle with radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.calculateArea());
+        System.out.println("Circumference: " + circle.calculateCircumference());
+    }
+}
